256f42a02471b2d7c729ade08619ab6b71a317c5
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-06-07 13:55:13 +0200

    ff

--- src/main/java/org/atemsource/atem/impl/hibernate/HibernateAccessor.java
+++ src/main/java/org/atemsource/atem/impl/hibernate/HibernateAccessor.java
@@ -1,20 +1,11 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.impl.hibernate;
-
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -54,27 +45,6 @@ public class HibernateAccessor implements Accessor
 		{
 			field.setAccessible(true);
 		}
-	}
-
-	@Override
-	public <A extends Annotation> A getAnnotation(Class<A> annotationClass)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Annotation getAnnotationAnnotatedBy(Class<? extends Annotation> annotationClass)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Collection<? extends Annotation> getAnnotations()
-	{
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public Field getField()
