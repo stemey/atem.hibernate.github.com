dfc2748724ccbf15fee22a550bd8b8c8fafd733d
Stefan Meyer <stefan-meyer@sinnerschrader.com>
2012-06-12 15:34:31 +0200

    fixed test

--- src/main/java/org/atemsource/atem/impl/hibernate/CalendarTypeRegistrar.java
+++ src/main/java/org/atemsource/atem/impl/hibernate/CalendarTypeRegistrar.java
@@ -1,32 +1,22 @@
 /*******************************************************************************
- * Stefan Meyer, 2012
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Stefan Meyer, 2012 Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 package org.atemsource.atem.impl.hibernate;
-
 
 import java.util.Calendar;
 
 import org.atemsource.atem.api.attribute.primitive.DateType.Precision;
 import org.atemsource.atem.api.type.PrimitiveType;
-import org.atemsource.atem.api.type.PrimitiveTypeRegistrar;
 import org.atemsource.atem.impl.common.attribute.primitive.DateTypeImpl;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class CalendarTypeRegistrar implements PrimitiveTypeRegistrar
+public class CalendarTypeRegistrar implements org.atemsource.atem.spi.PrimitiveTypeRegistrar
 {
 
 	@Override
